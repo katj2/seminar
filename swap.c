22c7aff609e84e5fd003168b22ad8053b7a31a15
katj2 <114476806+katj2@users.noreply.github.com>
2023-11-08 11:14:37 +0300

    Create swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 28;
+    int y = 50;
+
+    print("x is %i\n", is x);
+    print("y is %i\n", is y);
+
+    print("x is now %i\n", is x);
+    print("y is now %i\n", is y);
+}
