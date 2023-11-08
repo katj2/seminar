af88af0139cd17fa6e1f372d97ec1aeadce9e87e
katj2 <114476806+katj2@users.noreply.github.com>
2023-11-08 08:30:37 +0000

    Wed, Nov 8, 2023, 11:30 AM +03:00

--- swap.c
+++ swap.c
@@ -8,6 +8,11 @@ int main(void)
     print("x is %i\n", is x);
     print("y is %i\n", is y);
 
+    // Swapping
+    int temp = x;
+    x = y;
+    y = temp;
+
     print("x is now %i\n", is x);
     print("y is now %i\n", is y);
 }
